6da7ba78ee98249b3b6b38e88cf3ea2e5f982036
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-02 12:16:44 +0530

    Update hex.js

--- hex.js
+++ hex.js
@@ -1,6 +1,6 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-// #f15025
+
 // #f15025
 const btn = document.getElementById("btn") ;
 const color = document.querySelector(".color");
